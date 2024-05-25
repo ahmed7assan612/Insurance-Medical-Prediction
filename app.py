f2f4f2da3cc83adc7bccbd62d6a35d1e6a92e9e8
Ahmed Hassan <109879993+ahmed7assan612@users.noreply.github.com>
2024-05-25 19:47:09 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -2,9 +2,8 @@ import numpy as np
 from flask import Flask, request, jsonify, render_template
 import pickle
 
-# Create flask app
 flask_app = Flask(__name__)
-model = pickle.load(open("model.pkl", "rb"))
+model = pickle.load(open("KNN_model.pkl", "rb"))
 
 @flask_app.route("/")
 def welcome():
@@ -15,7 +14,7 @@ def predict():
     float_features = [float(x) for x in request.form.values()]
     features = [np.array(float_features)]
     prediction = model.predict(features)
-    return render_template("index.html", prediction_text = "The flower species is {}".format(prediction))
+    return render_template(prediction_text = "The Answer {}".format(prediction))
 
 if __name__ == "__main__":
     flask_app.run(debug=True)
